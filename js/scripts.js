94cfd7c1dcd3689c76b94564db4430f07749489a
Gideon Marinho Gonçalves <gimagodev@gmail.com>
2018-07-01 23:12:02 -0300

    first commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,86 @@
+const webApiDomain = 'http://localhost:3000'
+
 $( document ).ready( function() {
+
+    loadTable();
+
     $( '#divListagem' ).hide()
 
     $( '#btnListar,#btnCadastrar' ).click( function() {
-        $( '#divListagem,#divCadastro' ).toggle()
+        $( '#divListagem, #divCadastro' ).toggle()
     })
 
     $( 'form' ).submit( function( event ) {
         const data = $( this ).serializeArray()
 
-        updateDatabase( data, updateTable(data) )
+       // updateDatabase( data, updateTable(data) )
+        updateDatabase( data, loadTable )
+        $( '#divListagem, #divCadastro' ).toggle()
         event.preventDefault()
     })
 
-    // $( 'table' ).on( 'click', 'input[ value = "X" ]', function() {
-    //     $( this ).closest( 'tr' ).remove()
-
-    // })
+    $('table').on('click', 'input[value="X"]', function() {
+        if( confirm( 'Tem certeza que deseja excluir este cliente?')) {
+            const input = $(this)
+            const id = input.attr('data-id')
+            deleteCustomer(id, function() {
+                input.closest( 'tr' ).remove()
+            })
+        }
+    } )
 
 })
 
+/*
 function updateTable( data ) {
+    
     let linha = '' 
     data.forEach( item => linha += '<td>' + item.value + '</td>' )
 
     if( $( 'table > tbody > tr > td').length === 1 ) // se tem apenas uma TD, é a default
-                $( 'table > tbody' ).empty()
+        $( 'table > tbody' ).empty();
 
     $( 'table > tbody' ).append( '<tr>' + linha + '<td> <input type = "button" value = "X" /></td></tr>')
     $( '#divListagem,#divCadastro').toggle()
 
 }
+*/
 
-const webApiDomain = 'http://localhost:3000'
 function updateDatabase( data, callback ) {
 
     const json = {}
-    data.forEach( item => json[item['name']] = item['value'])
+    data.forEach( item => json[ item[ 'name' ]] = item[ 'value' ])
 
-    $.post(webApiDomain = '/clientes', json, function( data ) {
+    $.post(webApiDomain + '/clientes', json, function( data ) {
 
         alert('Cliente cadastrado com sucesso!')
         callback( data )
     })
 
 }
+
+function loadTable() {
+    const tbody = $( 'table > tbody' )
+    tbody.empty()
+
+    $.getJSON( webApiDomain + '/clientes', function( data ) {
+        data.forEach( item => {
+            let linha = '<td>' + item.nome + '</td><td>' + 
+            item.idade + '</td><td>' + item.uf + '</td>'
+            tbody.append( '<tr>' + linha + '<td><input type="button" value="X" data-id="'+item._id+'"/></td></tr>')
+        })
+    })
+}
+
+function deleteCustomer( id, callback ) {
+    $.ajax ( {
+        url     : webApiDomain + '/clientes/' + id,
+        method  : 'DELETE',
+        success : function( result ) {
+            alert(' Cliente excluído com sucesso-aqui ')
+            callback()
+        }
+    })
+}
+
+
